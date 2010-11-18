0339110ba03948d36e028fffea0b989405033fd4
Relistan <karl@matthias.org>
2010-11-17 20:09:17 -0800

    Remove mocha in favor of rspec built-ins

--- spec/ec2command_spec.rb
+++ spec/ec2command_spec.rb
@@ -1,6 +1,5 @@
 load File.expand_path(File.join(File.dirname(__FILE__), '..', 'ec2'))
 require 'yaml'
-require 'mocha'
 require 'stringio'
 require 'mocks'
 
@@ -10,14 +9,12 @@ describe EC2Command do
 		@fake_entries = YAML.load_file(File.join(File.dirname(__FILE__), "fixtures", "info"))
 		$config = YAML.load_file(File.join(File.dirname(__FILE__), "fixtures", "ec2rc"))
 		sdb_query = YAML.load_file(File.join(File.dirname(__FILE__), "fixtures", "sdb_query"))
-		SDB.any_instance.stubs(:get_attributes => @fake_entries['i-19028f70'], 
-				:get_all => sdb_query,
-				:delete_attributes => nil,
-				:put_attributes => nil,
-				:sdb_domain => nil)
 		$all_instances = EC2Instances.new
+		$all_instances.sdb.stub!(:get_attributes).and_return(@fake_entries['i-19028f70'])
+		$all_instances.sdb.stub!(:get_all).and_return(sdb_query)
+		$all_instances.stub!(:info).and_return(@fake_entries)
+		$all_instances.stub!(:get_instance).and_return(@fake_entries['i-19028f70'])
 		$all_instances.flush # No caching!
-		$all_instances.stubs(:info).returns(@fake_entries)
 		@command = EC2Command.new
 	end
 
@@ -36,27 +33,22 @@ describe EC2Command do
 	
 	it "should list all instances" do
 		@command.list([])
-		EC2Instance.any_instance.stubs(:key).returns("somekey.pem")
 		$io.string.should include('blog')
 	end
 
 	it "should connect over ssh" do
-		$all_instances.stubs(:get_instance).returns(@fake_entries['i-19028f70'])
-		EC2Instance.any_instance.stubs(:key).returns("somekey.pem")
+		File.should_receive(:exist?).with("/tmp/ubuntu-images").and_return(true)
 		@command.ssh(["blog"])
-		$io.string.should == "ssh -i somekey.pem ubuntu@ec2-14-43-96-210.compute-1.amazonaws.com \n"
+		$io.string.should == "ssh -i /tmp/ubuntu-images ubuntu@ec2-14-43-96-210.compute-1.amazonaws.com \n"
 	end
 
 	it "should copy files over scp" do
-		$all_instances.stubs(:get_instance).returns(@fake_entries['i-19028f70'])
-		EC2Instance.any_instance.stubs(:key).returns("somekey.pem")
+		File.should_receive(:exist?).with("/tmp/ubuntu-images").and_return(true)
 		@command.scp(["somefile", "ubuntu@blog:sadf"])
 		$io.string.should_not include("blog")
 	end
 
 	it "should launch a web browser" do
-		$all_instances.stubs(:get_instance).returns(@fake_entries['i-19028f70'])
-		EC2Instance.any_instance.stubs(:key).returns("somekey.pem")
 		@command.http(["blog"])
 		$io.string.should include("http://#{@fake_entries['i-19028f70'].dns_name}")
 	end
@@ -66,6 +58,5 @@ describe EC2Command do
 		@command.config([])
 		$io.string.should include("vim")
 	end
-
 
 end
